1dfde330de306137bd5efacdd6da3037bd56b54e
Omer21200 <omerbenitez212000@gmail.com>
2024-07-02 16:44:15 -0500

    Subida de las tablas Estado Civil, Etnia, Genero, Lugar, Nacionalidad, Provincia y  Tipo Lugar

---
+++ Base de Datos/Subida de Datos/Nacionalidad/nacionalidad.js
@@ -0,0 +1,89 @@
+const fs = require('fs');
+const mysql = require('mysql2/promise');
+const { exit } = require('process');
+const xlsx = require('xlsx');
+let dat = [];
+let dataExcel;
+
+async function conectarYInsertarRegiones(dat) {
+    try {
+        const connection = await mysql.createConnection({
+            host: '127.0.0.1',
+            user: 'root',
+            password: '1234',
+            database: 'prin'
+        });
+
+        for (const da of dat) {
+            try {
+
+                const query = `INSERT INTO nacionalidad (tipo_nacionalidad) VALUES ('${da.tipo_nacionalidad}');`;
+                await connection.query(query); // Utiliza connection.query en lugar de connection.execute
+                console.log(`Detencion "${da.tipo_nacionalidad}" insertada correctamente.`);
+                agregarDatosAlArchivo('nacionalidad.txt', `${query}\n`);
+            } catch (error) {
+                console.error(`${da}`, error);
+                exit();
+            }
+        }
+        connection.end(); // Finaliza la conexión después de completar todas las inserciones
+    } catch (error) {
+        console.error('Error al conectar a la base de datos:', error);
+        exit();
+    }
+}
+function agregarDatosAlArchivo(rutaArchivo, datosAInsertar) {
+    fs.appendFile(rutaArchivo, datosAInsertar, (error) => {
+        if (error) {
+            console.error('Error al agregar datos al archivo:', error);
+            return;
+        } else {
+            console.log('Datos agregados al archivo correctamente.');
+            // Cerrar el archivo después de terminar de escribir en él
+        }
+    });
+}
+
+function leerXcel(ruta) {
+    const workbook = xlsx.readFile(ruta);
+    const workbookSheets = workbook.SheetNames;
+    const sheet = workbookSheets[2];
+    dataExcel = xlsx.utils.sheet_to_json(workbook.Sheets[sheet]);
+
+    for (const itemfila of dataExcel) {
+        /*
+        const rawMatchDate = itemfila['Fecha_de_Detencion'];
+        let formattedDate = rawMatchDate;
+        let Edad = itemfila['Edad'];
+        
+
+        if(typeof Edad ==='string'){
+            Edad=0;
+        }
+
+        if (typeof rawMatchDate === 'number') {
+            const dateObject = xlsx.SSF.parse_date_code(rawMatchDate);
+            formattedDate = `${dateObject.y}-${dateObject.m}-${dateObject.d}`;
+        }
+        */
+
+        const pa = {
+            tipo_nacionalidad: itemfila['tipo_nacionalidad']
+        };
+        dat.push(pa);
+    }
+
+    //console.log(dat)
+
+}
+
+
+
+//leerXcel('pru.xlsx');
+//conectarYInsertarRegiones(dat);
+
+leerXcel('2016.xlsx')
+conectarYInsertarRegiones(dat);
+//console.log(dat);
+//const tor= buscarTorneo("1930 FIFA Men's World Cup");
+//console.log(tor);
